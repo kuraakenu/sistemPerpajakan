23bea913b7305eaf9be9330fa2e0eba51fb46bf3
Kuraakenu <krakenxg23@gmail.com>
2025-03-03 22:30:14 +0700

    fixing register

--- sistemPerpajakan.cpp
+++ sistemPerpajakan.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
+#include <cstdlib>
+#include <cctype>
 
 using namespace std;
 string usnTemp, passTemp;
-string usn = "a";
-string pass = "b";
+string usn = "admin";
+string pass = "admin";
 
 void welcome();
 bool login(bool success);
 bool regist(bool success);
+bool isUsnValid();
+bool isPassValid();
+bool isBlank(string str);
 
 struct dataKendaraan{
     string brandKendaraan;
@@ -25,7 +30,7 @@ int main(){
 void welcome(){
     char choice;
     bool success = false;
-
+ 
     do{
         system("cls");
         cout << "===| Selamat Datang! |===\n";
@@ -62,7 +67,7 @@ bool login(bool success){
             cout << "===| Login Berasil! |===\n";
             cout << "Anda akan diarahkan ke halaman utama!\n";
             system("pause");
-            return success = true;
+            return true;
         }else{
             system("cls");
             coba -= 1;
@@ -77,16 +82,94 @@ bool login(bool success){
     system("pause");
 }
 
-bool regist(bool success){
+bool regist(bool success){\
+    bool valid = false;
+    bool blank = true;
     system("cls");
-    cout << "Silakan Registrasi Terlebih Dahulu!\n";
-    cout << "Masukkan Username: ";
-    cin >> usn;
-    cout << "Masukkan Password: ";
-    cin >> pass;
+    cin.ignore();
+    while(!valid || blank){
+        system("cls");
+        cout << "Silakan Registrasi Terlebih Dahulu!\n";
+        cout << "Masukkan Username: ";
+        getline(cin, usn);
+        blank = isBlank(usn);
+        if(blank){
+            cout << "Username tidak boleh kosong!\n";
+            system("pause");
+        }
+        valid = isUsnValid();
+        if(!valid){
+            cout << "Username tidak boleh ada spasi atau lebih dari 12 char!\n";
+            system("pause");
+        }
+    }
+    valid = false;
+    blank = true;
+    while(!valid || blank){
+        system("cls");
+        cout << "Silakan Registrasi Terlebih Dahulu!\n";
+        cout << "Masukkan Username: " << usn << '\n';
+        cout << "Masukkan Password: ";\
+        getline(cin, pass);
+        blank = isBlank(pass);
+        if(blank){
+            cout << "Password tidak boleh kosong!\n";
+            system("pause");
+        }
+        valid = isPassValid();
+        if(!valid){
+            cout << "Password harus terdiri dari Huruf dan Angka tanpa Spasi!\n";
+            system("pause");
+        }
+    }
+
     system("cls");
     cout << "===| Registrasi Berhasil! |===\n";
     cout << "Anda akan diarahkan ke halaman login!\n";
     system("pause");
-    return success = true;
+    return true;
+}
+
+bool isUsnValid(){
+    if(usn.length() > 12){
+        return false;
+    }
+    for(char c : usn){
+        if(isspace(c)){
+            return false;
+        }
+    }
+    return true;
+}
+
+bool isPassValid(){
+    bool hasDigit = false;
+    if(pass.length() > 10){
+        return false;
+    }
+    for(char c : pass){
+        if(isspace(c)){
+            return false;
+        }
+        if(isdigit(c)){
+            hasDigit = true;
+        }
+    }
+    if(hasDigit){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+bool isBlank(string str){
+    if(str.empty()){
+        return true;
+    }
+    for(char c : str){
+        if(!isspace(c)){
+            return false;
+        }
+    }
+    return true;
 }
